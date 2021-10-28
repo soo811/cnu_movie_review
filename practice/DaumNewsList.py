599efd3cd5e572a6c96f610aa4668817f2a27c30
0811j <0811jisoo@gmail.com>
2021-10-28 14:52:18 +0900

    Daum News 목록 -> 제목 본론 수집

---
+++ practice/DaumNewsList.py
@@ -0,0 +1,26 @@
+# WebCrawling
+# -> Daum News 목록 -> 여러건의 기사와 본문 수집
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://news.daum.net/breakingnews/digital'
+result = requests.get(url)
+
+doc = BeautifulSoup(result.text,'html.parser')
+url_list = doc.select('ul.list_news2 a.link_txt')
+for i, url in enumerate(url_list):
+    print('■■ NEWS -> {}번 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■'.format(i+1))
+    new_url = url['href']
+    print('# URL:{}'.format(new_url))
+    result = requests.get(new_url)
+    doc = BeautifulSoup(result.text, 'html.parser')
+    title = doc.select('h3.tit_view')[0].get_text()
+    contents = doc.select('section p')
+    contents.pop(-1)  # 기자 정보 삭제
+    content = ''  # 본문 총합
+    for info in contents:
+        content += info.get_text()
+
+    print('# 뉴스 제목:{}'.format(title))
+    print('# 뉴스 본문:{}'.format(content))
