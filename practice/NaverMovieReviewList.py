02f87756f6bde585f319b0ce7e2b1286f286a388
0811j <0811jisoo@gmail.com>
2021-11-04 15:18:36 +0900

    NAVER MOVIE REVIEW 수집(리뷰, 점수, 작성자, 날짜) 완료

--- practice/NaverMovieReviewList.py
+++ practice/NaverMovieReviewList.py
@@ -18,18 +18,7 @@ for i, one in enumerate(review_list):
     score = one.select('div.star_score > em')[0].get_text()
 
     # 리뷰 정보 수집
-    review = one.select('div.score_reple > p > span')
-
-
-    # if len(review) == 2:
-    #   review_txt = review[1].get_text().strip()
-    # elif len(review) == 1:
-    #   review_txt = review[0].get_text().strip()
-
-    j = 0
-    if len(review) == 2:    # +관람객
-        j = 1
-    review_txt = review[j].get_text().strip()
+    review = one.select('div.score_reple > p > span')[-1].get_text().strip()
 
     # 작성자(닉네임) 정보 수집
     original_writer = one.select('div.score_reple dt em')[0].get_text().strip()
@@ -43,7 +32,7 @@ for i, one in enumerate(review_list):
     # yyyy.MM.dd 전처리 코드 작성
     date = original_date[0:10]
 
-    print(':: REVIEW -> {}'.format(review_txt))
+    print(':: REVIEW -> {}'.format(review))
     print(':: WRITER -> {}'.format(writer))
     print(':: SCORE -> {}'.format(score))
     print(':: DATE -> {}'.format(date))
